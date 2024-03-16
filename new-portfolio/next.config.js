c3a5f1d76b9f3d41fdc73ec9db3fd12f50420f85
Nihilus888 <Austonmartin@gmail.com>
2024-03-16 18:10:36 +0800

    Update next.config.js

--- new-portfolio/next.config.js
+++ new-portfolio/next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/new-portfolio",
-    output: "export",  // <=== enables static exports
-    reactStrictMode: true,
-  };
-  
-  module.exports = nextConfig;
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;

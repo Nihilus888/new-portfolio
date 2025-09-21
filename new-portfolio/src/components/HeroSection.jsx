8d8384bbc3b16190728205d4398bfa66b2dde978
NAME SURNAME <austonmartin@gmail.com>
2025-09-21 15:49:42 +0800

    update resume

--- new-portfolio/src/components/HeroSection.jsx
+++ new-portfolio/src/components/HeroSection.jsx
@@ -44,7 +44,7 @@ const HeroSection = () => {
                         </button>
                         <button className="px-1 py-1 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-500 text-white mt-3">
                             <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
-                            <a href="/Resume-Elliott-Tay.pdf"
+                            <a href="/public/Resume-Elliott-Tay.pdf.pdf"
                                 download
                             >
                                 Download CV
